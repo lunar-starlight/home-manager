09045715f32f2bb1317da997f40637c701d57e94
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2026-01-10 18:39:18 +0100

    moved marginal tool config out of default.nix

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,33 +1,13 @@
 { pkgs, ... }:
-let
-  marginaltool = import ./marginaltool.nix { inherit pkgs; };
-in
 {
+  imports = [
+    ./marginaltool.nix
+  ];
+
   home.packages = with pkgs; [
     (import ./getbib.nix { inherit pkgs; })
     (import ./switch-audio-device.nix { inherit pkgs; })
     (import ./brightness-plus-five.nix { inherit pkgs; })
     (import ./brightness-minus-five.nix { inherit pkgs; })
-    marginaltool
   ];
-
-  # MarginalTool
-  xdg = {
-    mimeApps = {
-      enable = true;
-      defaultApplications = {
-        "x-scheme-handler/bc-digsign" = "marginaltool.desktop";
-      };
-    };
-    desktopEntries.marginaltool = {
-      name = "MarginalTool";
-      exec = "${marginaltool}/bin/marginaltool";
-    };
-  };
-  home.file.".config/marginaltool".text = ''
-    [https://gcsign.uni-lj.si/BCSign/]
-    engine = file
-    keyfile = .ssh/sigenca.key.pem
-    certfile = .ssh/sigenca.crt.pem
-  '';
 }
--- scripts/marginaltool.nix
+++ scripts/marginaltool.nix
@@ -1,11 +1,35 @@
 { pkgs, ... }:
-pkgs.stdenv.mkDerivation {
-  name = "marginaltool";
-  propagatedBuildInputs = [
-    (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [
-      requests
-    ]))
-  ];
-  dontUnpack = true;
-  installPhase = "install -Dm755 ${./src/marginaltool.py} $out/bin/marginaltool";
+let
+  marginaltool =pkgs.stdenv.mkDerivation {
+    name = "marginaltool";
+    propagatedBuildInputs = [
+      (pkgs.python3.withPackages (pythonPackages: with pythonPackages; [
+        requests
+      ]))
+    ];
+    dontUnpack = true;
+    installPhase = "install -Dm755 ${./src/marginaltool.py} $out/bin/marginaltool";
+  };
+in {
+  home.packages = with pkgs; [ (import ./marginaltool.nix { inherit pkgs; }) ];
+
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/bc-digsign" = "marginaltool.desktop";
+      };
+    };
+    desktopEntries.marginaltool = {
+      name = "MarginalTool";
+      exec = "${marginaltool}/bin/marginaltool";
+    };
+  };
+  
+  home.file.".config/marginaltool".text = ''
+    [https://gcsign.uni-lj.si/BCSign/]
+    engine = file
+    keyfile = .ssh/sigenca.key.pem
+    certfile = .ssh/sigenca.crt.pem
+  '';
 }
