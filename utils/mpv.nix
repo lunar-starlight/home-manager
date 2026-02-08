82a65aa15939ffbc56e33fb949ea6e283ef691cb
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2026-02-08 12:28:52 +0100

    add mpv

--- utils/mpv.nix
+++ utils/mpv.nix
@@ -1,6 +1,35 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  programs.yt-dlp = {
+    enable = true;
+  };
+
   programs.mpv = {
     enable = true;
+
+    config = {
+      ytdl-format = "bestvideo[height<=?720][fps<=?30][vcodec!=?vp9]+bestaudio/best";
+    };
+
+    scriptOpts = {
+      ytdl_hook = {
+        ytdl_path = "${pkgs.yt-dlp}/bin/yt-dlp";
+      };
+    };
+  };
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "application/mpegurl" = "mpv.desktop";
+        "application/x-mpegurl" = "mpv.desktop";
+        "audio/mpegurl" = "mpv.desktop";
+        "audio/x-mpegurl" = "mpv.desktop";
+      };
+    };
+    #desktopEntries.mpv = {
+    #  name = "mpv";
+    #  exec = "${pkgs.mpv}/bin/mpv";
+    #};
   };
 }
