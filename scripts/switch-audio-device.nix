5b8af4b3b768cc74b1022198c13fa6e7a4e10ab4
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2026-07-17 13:28:25 +0200

    audio: named audio devices semantically

--- scripts/switch-audio-device.nix
+++ scripts/switch-audio-device.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }:
 pkgs.writeShellScriptBin "switch-audio-device" ''
-HEADPHONES="alsa_output.pci-0000_09_00.3.HiFi__Line1__sink"
-SPEAKERS="alsa_output.pci-0000_09_00.3.HiFi__Headphones__sink"
-HDMI="alsa_output.pci-0000_07_00.1.hdmi-stereo-extra4"
+BACK="alsa_output.pci-0000_09_00.3.HiFi__Line1__sink"
+FRONT="alsa_output.pci-0000_09_00.3.HiFi__Headphones__sink"
+HDMI="alsa_output.pci-0000_07_00.1.hdmi-stereo-extra3"
+HEADPHONES=$BACK
+SPEAKERS=$HDMI
 
 CURRENT_OUTPUT=($(${pkgs.pulseaudio}/bin/pactl get-default-sink))
-if [[ $CURRENT_OUTPUT = $SPEAKERS ]]
+if [[ $CURRENT_OUTPUT = $HEADPHONES ]]
 then
-        NEW_OUTPUT=$HDMI
+        NEW_OUTPUT=$SPEAKERS
 else
-	NEW_OUTPUT=$SPEAKERS
+	NEW_OUTPUT=$HEADPHONES
 fi
 ${pkgs.pulseaudio}/bin/pactl set-default-sink $NEW_OUTPUT
 ''
